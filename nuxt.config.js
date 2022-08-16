2083657b6571ea0fefbd45a05a9ce0a52b2ed2f8
imprisoner <continentalresident@yandex.ru>
2022-08-17 01:55:13 +0400

    moved fonts to static folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/fonts.css'
+    '~/static/fonts.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
