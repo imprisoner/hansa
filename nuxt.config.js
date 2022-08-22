e046e7be8cd8df1d5dbc1f39debeed3531333153
imprisoner <continentalresident@yandex.ru>
2022-08-22 21:30:50 +0400

    cdn fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,14 @@ export default {
     ],
     link: [
       // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      // {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat&display=swap&subset=cyrillic'}
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat&display=swap&subset=cyrillic'}
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/static/fonts.css'
-  ],
+  // css: [
+  //   '~/static/fonts.css'
+  // ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
