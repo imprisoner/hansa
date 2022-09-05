308ba6c95af9cbf5f71d87b27e17aad5cd2a4d0e
imprisoner <continentalresident@yandex.ru>
2022-09-05 22:50:00 +0400

    homepage swiper refactored added

---
+++ components/home/products-slider.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="swiper" :class="containerClass">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="(product, i) in products" :key="i">
+        <product-card :product="product" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["products", "config", "containerClass"],
+  data() {
+    return {
+      slider: null,
+    };
+  },
+  mounted() {
+      this.slider = new this.$swiper(`.${this.containerClass}`, {
+        ...this.config,
+        modules: [this.$swiperModules.Autoplay]
+      });
+  },
+};
+</script>
+
+<style scoped>
+.swiper .product-card {
+  box-sizing: border-box;
+  margin: 0 auto;
+}
+.swiper > .swiper-wrapper {
+  padding: 5px 0;
+}
+</style>
