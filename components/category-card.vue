66e1367f721013d46224a06108b7cc50b30023f5
imprisoner <continentalresident@yandex.ru>
2022-09-11 14:01:25 +0400

    final

---
+++ components/category-card.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card>
+    <div
+      class="d-flex flex-column flex-sm-row flex-no-wrap justify-space-between"
+    >
+      <div>
+        <nuxt-link :to="`/catalog/${cat.id}`" no-prefetch>
+          <v-card-title> {{ cat.title }} </v-card-title>
+        </nuxt-link>
+        <v-divider></v-divider>
+        <v-list>
+          <v-list-item v-for="(link, j) in cat.links" :key="j">
+            {{ link.label }}
+          </v-list-item>
+        </v-list>
+      </div>
+      <div>
+        <v-avatar tile :size="250" class="pa-4">
+          <v-img :src="cat.image.url" max-height="234" contain />
+        </v-avatar>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["cat"],
+};
+</script>
+
+<style scoped>
+a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
